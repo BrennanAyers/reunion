14d0291ee2caa2c07930bf4814f1044afb778493
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-10 08:41:17 -0600

    Add split cost test to Activity

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -33,4 +33,11 @@ class ActivityTest < Minitest::Test
     @activity.add_participant("Luther", 40)
     assert_equal 60, @activity.total_cost
   end
+
+  def test_it_returns_a_split_cost
+    @activity.add_participant("Maria", 20)
+    @activity.add_participant("Luther", 40)
+
+    assert_equal 30, @activity.split
+  end
 end
