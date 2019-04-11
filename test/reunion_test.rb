b8af4c89bbd7871f5c1e8307d38a558d9b6705ce
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-10 20:01:40 -0600

    Add initial Reunion tests

---
+++ test/reunion_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/activity'
+require './lib/reunion'
+class ReunionTest < Minitest::Test
+  def setup
+    @reunion = Reunion.new("1406 BE")
+    @brunch = Activity.new("Brunch")
+  end
+
+  def test_it_exists
+    assert_instance_of Reunion, @reunion
+  end
+
+  def test_it_has_a_name
+    assert_equal "1406 BE", @reunion.name
+  end
+
+  def test_it_starts_with_no_activities
+    assert_equal [], @reunion.activities
+  end
+end
